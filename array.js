c101dad0c11393624d39019851c64f68dff94fcd
onyoo <roberto9m9@gmail.com>
2016-01-31 18:14:23 +0100

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray() {
+  var array = new Array();
+  array.push("snickers", "hundred grand", "kitkat", "skittles");
+  return array;
+}
+
+function addElementToArray(array) {
+ array.push('A Christmas Story');
+return array;
+}
+
+function accessElementFromArray(array) {
+return array[2];
+}
+
+function replaceElementInArray(array) {
+  return array[1] = "Carter";
+}
+
+function removeElementFromArray(array) {
+  array.splice(2,2,"Roast Chicken");
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = new Array();
+  array.forEach(function(number){
+    newNums.push(number + 5);
+  });
+  return newNums;
+}
