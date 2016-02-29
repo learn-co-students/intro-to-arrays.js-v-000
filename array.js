1044dc4062b22a614a6c7871d39c3dd292f86147
Chen Yang <yangc5@mail.uc.edu>
2016-02-29 23:29:10 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,34 @@
 'use strict';
+
+ function createArray(){
+   return ["snickers", "hundred grand", "kitkat", "skittles"];
+ }
+
+function addElementToArray(array){
+  array.push('A Christmas Story');
+  return array;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  array[1]='Carter';
+  return array[1];
+}
+
+function removeElementFromArray(array){
+  array.splice(2,2);
+  array.push('Roast Chicken');
+  return array;
+}
+
+function iterateArray(array){
+  array.forEach(function(number, index){
+    array[index]=number+5;
+  });
+  return array;
+}
+
+// console.log(iterateArray([1, 2, 3]));
