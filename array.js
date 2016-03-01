751eedbe5141e3d8293c2f22f8a107ca86d92acb
Shana L Moore <shana.lavina.moore@gmail.com>
2016-03-01 10:02:27 -0800

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray (){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray (moviesArray){
+  moviesArray.push("A Christmas Story");
+  return moviesArray;
+}
+
+function accessElementFromArray (statesArray){
+  return statesArray[2];
+}
+
+function replaceElementInArray (namesArray){ 
+  namesArray[1] = "Carter";
+  return namesArray[1];
+}
+
+function removeElementFromArray (dishesArray){
+  dishesArray.splice(2, 2, "Roast Chicken");
+  return dishesArray;
+}
+
+function iterateArray (numbersArray){
+  var newNums = new Array();
+  numbersArray.forEach(function(number){
+    newNums.push(number + 5);
+  });
+  
+  return newNums;
+}
