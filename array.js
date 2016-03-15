c098142cb4a525af99d7928482e98576f1f9074d
Adam King <adam.king0126@gmail.com>
2016-03-15 11:00:06 -0500

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(arr) {
+  arr.push("A Christmas Story");
+  return arr
+}
+
+function accessElementFromArray(arr) {
+  return arr[2];
+}
+
+function replaceElementInArray(arr) {
+  arr[1] = "Carter";
+  return arr[1];
+}
+
+function removeElementFromArray(arr) {
+  var removeElements = arr.splice(2,2, "Roast Chicken");
+  return arr;
+}
+
+function iterateArray(array) {
+  array.forEach(function(element, index) {
+    array[index] = array[index] + 5;
+  });
+  return array;
+}
+
+
