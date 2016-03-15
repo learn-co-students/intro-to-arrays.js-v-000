e5b6e1102ec9a4ac94d3b862b275a6a2e2122b66
lisamarie616 <lisamarie616@gmail.com>
2016-03-15 14:44:28 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(movieArr) {
+  movieArr.push("A Christmas Story");
+  return movieArr;
+}
+
+function accessElementFromArray(statesArr) {
+  return statesArr[2];
+}
+
+function replaceElementInArray(namesArr) {
+  namesArr[1] = "Carter";
+  return namesArr[1];
+}
+
+function removeElementFromArray(dishesArr) {
+  dishesArr.splice(2, 2, "Roast Chicken");
+  return dishesArr;
+}
+
+function iterateArray(numbersArr) {
+  var newNums = [];
+  numbersArr.forEach(function(number) {
+    newNums.push(number + 5);
+  });
+  return newNums;
+}
