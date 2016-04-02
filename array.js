93b612a3a52751b0e4e20e0fcae60923a7f59bae
esimonian <eli.simonian@gmail.com>
2016-04-01 19:37:46 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(argument) {
+  argument.push("A Christmas Story");
+  return argument;
+}
+
+function accessElementFromArray(argument) {
+  return argument[2];
+}
+
+function replaceElementInArray(argument) {
+  return argument[1] = "Carter";
+}
+
+function removeElementFromArray(argument) {
+  argument.splice(2, 2, "Roast Chicken");
+  return argument;
+}
+
+function iterateArray(argument) {
+  var newNums = [];
+  argument.forEach(function(num){
+
+    newNums.push(num+5);
+  });
+  return newNums;
+
+}
