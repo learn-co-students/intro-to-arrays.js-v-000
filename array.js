3a18aab7d710f2555b13c4e8f0f5863d261165ed
pacosta613 <pedro.acosta613@gmail.com>
2016-04-04 18:06:48 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray() {
+   var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+   return array
+}
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+  return array
+}
+
+function accessElementFromArray(arrayOfStates){
+  return arrayOfStates[2];
+}
+
+function replaceElementInArray(arrayOfNames){
+  return arrayOfNames[1] = "Carter"
+}
+
+function removeElementFromArray(dishes){
+  dishes.splice(2, 3, "Roast Chicken");
+  return dishes
+}
+
+function iterateArray(array) {
+  var newA = [];
+  array.forEach(function(numbers) {
+    newA.push(numbers + 5);
+  })
+  return newA
+}
