a98bb1113de31bb7c66c863b774eb0c53ec10682
authorbeard <msnolan@gmail.com>
2016-04-23 17:35:44 -0400

    all tests passing

--- array.js
+++ array.js
@@ -1,1 +1,36 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+
+}
+
+function addElementToArray(array){
+  array.push('A Christmas Story');
+  return array;
+
+}
+
+function accessElementFromArray(array){
+  var third = array[2];
+  return third;
+
+}
+
+function replaceElementInArray(firstArray){
+  firstArray[1]="Carter";
+  return firstArray[1];
+}
+
+function removeElementFromArray(array){
+  array.splice(2, 2, 'Roast Chicken');
+  return array;
+}
+
+function iterateArray(array){
+  var newNums = [];
+  array.forEach(function(number, index){
+    newNums.splice(index, 1, number+5);
+  })
+  return newNums;
+}
