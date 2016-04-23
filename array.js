b96d300bc831f21ac805bf9809dac3a1be24dbfe
azheng249 <andy.zheng249@gmail.com>
2016-04-23 18:30:45 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function createArray () {
+  var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return array;
+}
+
+function addElementToArray (array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray (array) {
+  return array[2];
+}
+
+function replaceElementInArray (array) {
+  return array[1] = "Carter";
+}
+
+function removeElementFromArray (array) {
+  array.splice(2, 2, "Roast Chicken");
+  return array
+}
+
+function iterateArray (array) {
+  var newArray = [];
+
+  array.forEach(function(number, index) {
+    newArray[index] = number + 5;
+  });
+  
+  return newArray;
+}
