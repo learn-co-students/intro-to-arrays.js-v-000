1171978df2e9a32ee6fce15d47780574984f82f4
Saturn226 <serenehopes@gmail.com>
2016-04-27 02:10:24 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+ 
+}
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+  return array;
+
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  array[1] = "Carter";
+  return array[1];
+}
+
+function removeElementFromArray(array){
+  array.splice(2,2,"Roast Chicken");
+  return array
+}
+
+function iterateArray(array){
+  var newArray = [];
+  array.forEach(function(number){
+    newArray.push(number + 5);
+  });
+  return newArray;
+
+}
