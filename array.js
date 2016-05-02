e5fae0c76a11eb5151e32336aa6bf77bc6f986a9
cernanb <cernanb@gmail.com>
2016-05-01 22:53:09 -0700

    performed various array functions

--- array.js
+++ array.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function createArray(){
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+}
+
+function addElementToArray(arr){
+  arr.push("A Christmas Story");
+  return arr;
+}
+
+function accessElementFromArray(stateArray){
+  return stateArray[2];
+}
+
+function replaceElementInArray(nameArray){
+  return nameArray[1] = "Carter";
+}
+
+function removeElementFromArray(dishArray){
+  dishArray.splice(2,2, "Roast Chicken");
+  return dishArray;
+}
+
+function iterateArray(numArray){
+  var newNums = [];
+  numArray.forEach(function(num){
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
