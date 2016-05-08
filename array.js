9e1888fd87b88108151629ce9221494e521cfe35
Asher Sullivan <asherfolsomsullivan@gmail.com>
2016-05-08 13:11:55 -0400

    Done

--- array.js
+++ array.js
@@ -1,1 +1,32 @@
 'use strict';
+
+function createArray(){
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(movieArray){
+  movieArray.push("A Christmas Story");
+  return movieArray;
+}
+
+function accessElementFromArray(statesArray){
+  return statesArray[2];
+}
+
+function replaceElementInArray(namesArray){
+  namesArray[1] = "Carter";
+  return namesArray[1];
+}
+
+function removeElementFromArray(foodArray){
+  foodArray.splice(2, 2, "Roast Chicken");
+  return foodArray;
+}
+
+function iterateArray(numbersArray){
+  var newNumbers = [];
+  numbersArray.forEach(function(num){
+    newNumbers.push(num + 5);
+  });
+  return newNumbers;
+}
