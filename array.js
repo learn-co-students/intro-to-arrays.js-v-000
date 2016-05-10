f06a6076b788e2fef8bf88769accfcac8da3c914
LKWLaLa <lkwlala@gmail.com>
2016-05-10 16:20:10 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createArray(){
+  return new Array("snickers", "hundred grand", "kitkat", "skittles")
+
+}
+
+function addElementToArray(array){
+  array.push('A Christmas Story');
+  return array;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(array){
+  return array[1] = "Carter";
+}
+
+function removeElementFromArray(array){
+  array.splice(2, 2);
+  array.push("Roast Chicken");
+  return array;
+}
+
+function iterateArray(array){
+  var newNums = [];
+  array.forEach(function(i) {
+    i += 5;
+    newNums.push(i)
+  });
+  return newNums;
+}
+
