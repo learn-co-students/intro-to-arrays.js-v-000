11c01aa83940f5f6da5bc2fd9a07b6d8365bc777
genericlady <yiannakokalas@gmail.com>
2016-05-10 15:49:02 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,35 @@
 'use strict';
+
+function createArray() {
+  return [ 'snickers', 'hundred grand', 'kitkat', 'skittles' ];
+}
+
+function addElementToArray(elements) {
+  elements.push('A Christmas Story');
+  
+  return elements;
+}
+
+function accessElementFromArray(elements) {
+  return elements[2];
+}
+
+function replaceElementInArray(elements) {
+  return elements[1] = 'Carter'
+}
+
+function removeElementFromArray(elements) {
+  elements.splice(2, 2);
+  elements.push('Roast Chicken')
+
+  return elements;
+}
+
+function iterateArray(numbers) {
+  var newNums = [];
+  numbers.forEach(function(number) {
+    newNums.push(number+5)
+  });
+
+  return newNums;
+}
