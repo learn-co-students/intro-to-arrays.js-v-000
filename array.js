6f282fd8193fcd32925b9d8ea57ad8d3865debfd
Llubomir Stanojlovic <llustanojlovic@gmail.com>
2016-05-23 19:02:48 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray() {
+  var cat = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return cat;
+}
+
+function addElementToArray(movies){
+  movies.push("A Christmas Story");
+  return movies;
+}
+
+function accessElementFromArray(array){
+  return array[2];
+}
+
+function replaceElementInArray(thing) {
+    thing[1]="Carter";
+    return thing[1];
+}
+
+function removeElementFromArray(thing){
+  thing.splice(2,2,"Roast Chicken");
+  return thing;
+}
+
+function iterateArray(nums){
+  var newNums= [];
+  nums.forEach(function(aNum) {
+    newNums.push(aNum+5);
+  });
+  return newNums;
+}
