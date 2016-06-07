88197806c2f721614e12f649f2286173df8d1ec3
Aleta Lanier <aleta.marie3@gmail.com>
2016-06-07 15:46:40 -0500

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,36 @@
 'use strict';
+
+
+function createArray(){
+  var array = ["snickers","hundred grand","kitkat","skittles"];
+    return array
+}
+
+function addElementToArray(array){
+  array.push("A Christmas Story");
+     return array
+}
+
+function accessElementFromArray(arrayStates){
+	return (arrayStates)[2];
+}
+
+function replaceElementInArray(arrayNames){
+  arrayNames[1] = "Carter";
+  return arrayNames[1]
+ }
+
+ function removeElementFromArray(arrayDish){
+ 	arrayDish.splice(2, 2, "Roast Chicken");
+ 	 return arrayDish
+ }
+
+ function iterateArray(arrayNums){
+ 	var newNums = [ ];
+ 	   arrayNums.forEach(function(num){
+ 	  var add = num + 5;
+ 	     newNums.push(add);
+ 	     })
+ 	      return newNums
+ 	
+ }
