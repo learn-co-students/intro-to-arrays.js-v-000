c86f8b99a216c867f8af2dce7bd3abca2bd4d962
tejbans <singh.tejbans@gmail.com>
2016-06-12 14:06:14 -0700

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function createArray(){
+  var candies = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return candies;
+}
+
+function addElementToArray(movies){
+  movies.push("A Christmas Story");
+  return movies;
+}
+
+function accessElementFromArray(states){
+  return states[2];
+}
+
+function replaceElementInArray(names){
+  names[1] = "Carter"
+  return names[1];
+}
+
+function removeElementFromArray(dishes){
+  dishes.splice(2,2)
+  dishes.push("Roast Chicken")
+  return dishes;
+}
+
+function iterateArray(numberarr){
+  var newNums=[];
+  numberarr.forEach(function(num){
+    newNums.push(num + 5);
+  });
+  return newNums;
+}
