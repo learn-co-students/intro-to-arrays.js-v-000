d86658a4ee957aee92180c17302f0c2225966280
Nathaniel Miller <dukeoflaser@gmail.com>
2016-06-16 00:28:19 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,9 @@
 'use strict';
+function createArray(){
+  return ['snickers', 'hundred grand', 'kitkat', 'skittles']
+}
+
+function addElementToArray(array){
+  array.push('A Christmas Story');
+  return array;
+}
