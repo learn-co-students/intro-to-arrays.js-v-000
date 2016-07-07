ecec8c485d4a551c831884c2b634cc0770e0fc55
Peter Jacobson <github email address>
2016-07-07 14:46:19 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+  var candy = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return candy;
+}
+
+function addElementToArray(christmasMovies){
+christmasMovies.push("A Christmas Story");
+  return christmasMovies;
+}
+
+function accessElementFromArray(states){
+  return states[2];
+}
+
+function replaceElementInArray(names){
+  names[1] = "Carter";
+  return names[1];
+}
+
+function removeElementFromArray(dishes) {
+  dishes.splice(2, 2, "Roast Chicken");
+  return dishes;
+}
+
+function iterateArray(numbers) {
+var newNums = new Array();
+ numbers.forEach(function(letter){
+  newNums.push(letter + 5)
+ });
+  return newNums;
+}
