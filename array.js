2f232c8208c59060e4ebc96136e3a9a256ee7f19
Heber Sandoval <he3ers@gmail.com>
2016-07-19 18:30:06 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array
+}
+
+function accessElementFromArray(statesArray) {
+  return statesArray[2];
+}
+
+function replaceElementInArray(namesArray) {
+  return namesArray[1] = "Carter";
+}
+
+function removeElementFromArray(dishesArray) {
+  dishesArray.splice(2, 2, "Roast Chicken");
+  return dishesArray;
+}
+
+function iterateArray(numbers) {
+  var newNums = [];
+  numbers.forEach(function(number) {
+    newNums.push(number + 5);
+  });
+  return newNums;
+}
