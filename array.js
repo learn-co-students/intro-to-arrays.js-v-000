7a8f5f4da91a63fa2b742e5e745b0f8526beb1d6
Jason Casares <jasonc@csdrafting.com>
2016-07-26 22:12:56 +0000

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function createArray() {
+  var candy = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return candy;
+}
+
+function addElementToArray(array) {
+  var array = ["Home Alone", "Love Actually", "Elf"];
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  var array = ["New York", "California", "Virginia", "Louisiana", "Nevada"];
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  var array = ["Joe", "Tim", "Stephen", "Sarah", "Stephanie"];
+  array[1] = "Carter";
+  return array[1];
+}
+
+function removeElementFromArray(array) {
+  var array = ["Pasta Alfredo", "Grilled Salmon", "Hamburgers", "Black Bean Soup"];
+  array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(numbers) {
+  var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  var newNums = new Array();
+  numbers.forEach(function(numbers)) {
+    newNums.push([numbers + 5]);
+    return newNums;
+  });
+}
