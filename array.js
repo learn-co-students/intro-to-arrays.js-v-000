b1df253bc2032d4ad26f9dbb102c6104314f7881
glee38 <glee3388@gmail.com>
2016-07-28 00:06:48 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,34 @@
 'use strict';
+
+function createArray() {
+  var hellsYeah = ["snickers", "hundred grand", "kitkat", "skittles"];
+  return hellsYeah;
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array;
+}
+
+function accessElementFromArray(array) {
+  return array[2];
+}
+
+function replaceElementInArray(array) {
+  array[1] = "Carter";
+  return array[1];
+}
+
+function removeElementFromArray(array) {
+  var removedItems = array.splice(2, 2, "Roast Chicken");
+  return array;
+}
+
+function iterateArray(array) {
+  var newNums = [];
+  array.forEach(function(number) {
+    var newNum = number + 5;
+    newNums.push(newNum);
+  });
+  return newNums;
+}
