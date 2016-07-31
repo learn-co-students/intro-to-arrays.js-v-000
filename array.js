a591a26358050b40890cfd9184faaa70c4762fef
Bradley Reiss <bradleyreiss@Bradleys-MacBook-Air.local>
2016-07-31 12:52:17 -0400

    Done.

--- array.js
+++ array.js
@@ -1,1 +1,33 @@
 'use strict';
+
+function createArray(){
+	var array = ["snickers", "hundred grand", "kitkat", "skittles"];
+	return array;
+}
+
+function addElementToArray(arr){
+	arr.push("A Christmas Story");
+	return arr;
+}
+
+function accessElementFromArray(arr){
+	return arr[2];
+}
+
+function replaceElementInArray(arr){
+	arr[1] = "Carter";
+	return arr[1]
+}
+
+function removeElementFromArray(arr){
+	arr.splice(2,2, "Roast Chicken");
+	return arr;
+}
+
+function iterateArray(arr){
+	var newNums = new Array();
+	arr.forEach(function(number){
+		newNums.push(number+5);
+	});
+	return newNums;
+}
